f708da94ed8fdeef526cef98097619d92be0488c
ATMA1234 <atmananddevarvar@gmail.com>
2018-10-01 11:06:41 +0530

    initial commit 01/10/2018

--- Discon_Recon/src/main/java/com/androiddeft/jsonretrofit/Database/DatabaseHelper.java
+++ Discon_Recon/src/main/java/com/androiddeft/jsonretrofit/Database/DatabaseHelper.java
@@ -1,9 +1,11 @@
 package com.androiddeft.jsonretrofit.Database;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.androiddeft.jsonretrofit.Models.Discon_data;
+import com.androiddeft.jsonretrofit.Models.DisconnectionList;
 import com.androiddeft.jsonretrofit.Models.Recon_data;
 import com.androiddeft.jsonretrofit.Values.FunctionCall;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
@@ -12,75 +14,64 @@ import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
 import java.io.File;
-import java.sql.SQLException;
 
-public class DatabaseHelper {
-    private MyHelper mh;
-    private SQLiteDatabase sqLiteDatabase;
-    private static FunctionCall functionCall = new FunctionCall();
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+    private static SQLiteDatabase myDataBase;
+    private static FunctionCall fcall = new FunctionCall();
+    private static final String DATABASE_NAME = "discon_recon.db";
+    private static String path = fcall.filepath("database");
+    private final static String DATABASE_PATH = path + File.separator;
+    private static final int DATABASE_VERSION = 1;
     private Dao<Discon_data, Integer> disconDao;
     private Dao<Recon_data, Integer> reconDao;
 
-    private String databasepath = "";
-    private String databasefolder = "database";
-    private String database_name = "discon_recon.db";
-    private File databasefile = null;
-
-
+    //****************************************************************************************************************************************
     public DatabaseHelper(Context context) {
+        super(context, DATABASE_PATH + DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //****************************************************************************************************************************************
+    public void openDatabase() throws SQLException {
+        myDataBase = this.getWritableDatabase();
+    }
+
+    //****************************************************************************************************************************************
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
         try {
-            databasefile = functionCall.filestorepath(databasefolder, database_name);
-            databasepath = functionCall.filepath(databasefolder) + File.separator + database_name;
-            mh = new MyHelper(context, databasepath, null, 7);
-        } catch (Exception e) {
-            e.printStackTrace();
+            TableUtils.createTable(connectionSource, Discon_data.class);
+            TableUtils.createTable(connectionSource, Recon_data.class);
+        } catch (java.sql.SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void open() {
-        sqLiteDatabase = mh.getWritableDatabase();
+    //****************************************************************************************************************************************
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+
+//        try {
+//            TableUtils.dropTable(connectionSource, Discon_data.class, true);
+//            TableUtils.dropTable(connectionSource, Recon_data.class, true);
+//            onCreate(database, connectionSource);
+//        } catch (java.sql.SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 
-    public class MyHelper extends OrmLiteSqliteOpenHelper {
-
-        public MyHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, name, factory, version);
+    //****************************************************************************************************************************************
+    public Dao<Discon_data, Integer> getDisconDao() throws java.sql.SQLException {
+        if (disconDao == null) {
+            disconDao = getDao(Discon_data.class);
         }
+        return disconDao;
+    }
 
-        @Override
-        public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
-            try {
-                TableUtils.createTable(connectionSource, Discon_data.class);
-                TableUtils.createTable(connectionSource, Recon_data.class);
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+    //****************************************************************************************************************************************
+    public Dao<Recon_data, Integer> getReconDao() throws java.sql.SQLException {
+        if (reconDao == null) {
+            reconDao = getDao(Recon_data.class);
         }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
-            try {
-                TableUtils.dropTable(connectionSource, Discon_data.class, true);
-                TableUtils.dropTable(connectionSource, Recon_data.class, true);
-                onCreate(sqLiteDatabase, connectionSource);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public Dao<Discon_data, Integer> getDisconDao() throws SQLException {
-            if (disconDao == null) {
-                disconDao = getDao(Discon_data.class);
-            }
-            return disconDao;
-        }
-
-        public Dao<Recon_data, Integer> getReconDao() throws SQLException {
-            if (reconDao == null) {
-                reconDao = getDao(Recon_data.class);
-            }
-            return reconDao;
-        }
+        return reconDao;
     }
 }
